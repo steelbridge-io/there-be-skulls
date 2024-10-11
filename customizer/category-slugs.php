8ba15c2c55ce0ca9b6104acc8768e540ba878186
steelbridge-io <chris@steelbridge.io>
2024-10-10 23:10:56 -0700

    Add category slug customization to the theme
    Introduced a new file to manage custom category slugs in the theme. This allows users to define up to 12 category slugs via the theme customizer, enhancing flexibility in content organization.

---
+++ customizer/category-slugs.php
@@ -0,0 +1,17 @@
+<?php
+
+// common-config.php
+    $category_slugs = array(
+      get_theme_mod('skulls_category_1', 'clothing'),
+      get_theme_mod('skulls_category_2', 'jewelry'),
+      get_theme_mod('skulls_category_3', 'accessories'),
+      get_theme_mod('skulls_category_4', 'decorative'),
+      get_theme_mod('skulls_category_5', 'stone'),
+      get_theme_mod('skulls_category_6', 'tapestries'),
+      get_theme_mod('skulls_category_7', 'miscellaneous'),
+      get_theme_mod('skulls_category_8', 'sale'),
+      get_theme_mod('skulls_category_9', ''),
+      get_theme_mod('skulls_category_10', ''),
+      get_theme_mod('skulls_category_11', ''),
+      get_theme_mod('skulls_category_12', ''),
+    );
