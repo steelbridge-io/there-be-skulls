06e3473aca16617de413331b6509284f7a856bdd
steelbridge-io <chris@steelbridge.io>
2024-10-11 20:08:18 -0700

    Add customizer support for carousel items and links
    Created a new PHP file to integrate theme mod options for carousel items and their associated links. This enhancement allows users to customize up to four carousel items and links from the WordPress theme customizer.

---
+++ customizer/carousel-items-links.php
@@ -0,0 +1,16 @@
+<?php
+
+$carousel_items = array(
+	get_theme_mod( 'skulls_carousel_item_1', '' ),
+	get_theme_mod( 'skulls_carousel_item_2', '' ),
+	get_theme_mod( 'skulls_carousel_item_3', '' ),
+	get_theme_mod( 'skulls_carousel_item_4', '' ),
+);
+
+$carousel_links = array(
+	get_theme_mod( 'skulls_carousel_link_1', '' ),
+	get_theme_mod( 'skulls_carousel_link_2', '' ),
+	get_theme_mod( 'skulls_carousel_link_3', '' ),
+	get_theme_mod( 'skulls_carousel_link_4', '' ),
+);
+
