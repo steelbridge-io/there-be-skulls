57574e8999630605b164b5ed5f1f52ebc182f949
steelbridge-io <chris@steelbridge.io>
2024-10-15 00:48:42 -0700

    Update footer.php to use dynamic year and comment out social links
    Replaced hardcoded year with dynamic PHP date function to ensure the current year is always displayed. Commented out social media links section for future reactivation if needed.

--- footer.php
+++ footer.php
@@ -1,16 +1,18 @@
 <?php
-
+/**
+ * WordPres Footer
+ */
 ?>
 </div> <!-- /container-height -->
 </div>
 <footer class="bg-black py-4 text-center">
 	<div class="container">
-		<p class=" small mb-2">&copy; 2024 There Will Be Skulls. All Rights Reserved.</p>
-		<div class="d-flex justify-content-center gap-3">
+		<p class=" small mb-2">&copy; <?php echo date("Y"); ?> There Be Skulls. All Rights Reserved.</p>
+		<!--<div class="d-flex justify-content-center gap-3">
 			<a href="#" class="text-decoration-none">Facebook</a>
 			<a href="#" class="text-decoration-none">Instagram</a>
 			<a href="#" class="text-decoration-none">Twitter</a>
-		</div>
+		</div>-->
 	</div>
 </footer>
 
