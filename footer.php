1543ded05eca79dd9a6b97d42b6b44c13760f3e2
steelbridge-io <chris@steelbridge.io>
2024-10-20 23:19:19 -0700

    Update footer.php to add widgetized area
    Revised the footer to include three widgetized columns, allowing dynamic content insertion for more flexibility. Also cleaned up unnecessary comments and retained copyright information.

--- footer.php
+++ footer.php
@@ -1,19 +1,30 @@
-<?php
-/**
- * WordPres Footer
- */
-?>
-</div> <!-- /container-height -->
-</div>
 <footer class="bg-black py-4 text-center">
-	<div class="container">
-		<p class=" small mb-2">&copy; <?php echo date("Y"); ?> There Be Skulls. All Rights Reserved.</p>
-		<!--<div class="d-flex justify-content-center gap-3">
-			<a href="#" class="text-decoration-none">Facebook</a>
-			<a href="#" class="text-decoration-none">Instagram</a>
-			<a href="#" class="text-decoration-none">Twitter</a>
-		</div>-->
-	</div>
+  <div class="container">
+    <div id="footer-row" class="row">
+      <!-- Footer Column 1 -->
+      <div class="col-md-4">
+        <?php if (is_active_sidebar('footer-1')) : ?>
+          <?php dynamic_sidebar('footer-1'); ?>
+        <?php endif; ?>
+      </div>
+
+      <!-- Footer Column 2 -->
+      <div class="col-md-4">
+        <?php if (is_active_sidebar('footer-2')) : ?>
+          <?php dynamic_sidebar('footer-2'); ?>
+        <?php endif; ?>
+      </div>
+
+      <!-- Footer Column 3 -->
+      <div class="col-md-4">
+        <?php if (is_active_sidebar('footer-3')) : ?>
+          <?php dynamic_sidebar('footer-3'); ?>
+        <?php endif; ?>
+      </div>
+    </div>
+
+    <p class="small mb-2">&copy; <?php echo date("Y"); ?> There Be Skulls. All Rights Reserved.</p>
+  </div>
 </footer>
 
 <?php wp_footer(); ?>
