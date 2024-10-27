d9c69de1ebd99f2415794816b8a8ea882d9c2551
steelbridge-io <chris@steelbridge.io>
2024-10-26 23:27:10 -0700

    Add option to disable Slide In CTA in customizer
    Introduced a new setting and control for disabling the Slide In CTA feature via a checkbox in the theme customizer. This setting helps users easily control the visibility of the Slide In CTA element.

--- customizer/general-customizer.php
+++ customizer/general-customizer.php
@@ -12,5 +12,18 @@ function therebeskulls_custom_logo_setup($wp_customize) {
     'section'       => 'title_tagline',
     'settings'      => 'custom_logo',
   )));
+
+  $wp_customize->add_setting('disable_custom_js', array(
+    'default'       => false,
+    'transport'     => 'refresh',
+  ));
+
+  // Add a control for custom js checkbox
+  $wp_customize->add_control('disable_custom_js', array(
+    'label'         => __('Disable Slide In CTA', 'mytheme'),
+    'section'       => 'title_tagline',
+    'settings'      => 'disable_custom_js',
+    'type'          => 'checkbox',
+  ));
 }
 add_action('customize_register', 'therebeskulls_custom_logo_setup');
